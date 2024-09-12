1ff900490903a2bec50c8a9c91a7a866dfd32d35
Lugi-PL <lugowskibartek8@gmail.com>
2024-09-12 22:48:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Bot by Lugi.pl", "Dc BOTY pisz pv: Lugi.pl"];
+const statusMessages = ["Bot made by Lugi.pl", "Dc BOTY pisz pv: Lugi.pl"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
